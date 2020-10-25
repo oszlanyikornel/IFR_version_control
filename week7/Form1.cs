a2d26cfe085838f4b772486842909c40f123e575
oszlanyikornel <kornel.oszlanyi@stud.uni-corvinus.hu>
2020-10-25 16:00:11 +0100

    Added simulation

--- week7/Form1.cs
+++ week7/Form1.cs
@@ -17,6 +17,9 @@ namespace week7
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        List<PopulationData> popData = new List<PopulationData>();
+
+        Random rng = new Random(1234);
 
         public Form1()
         {
@@ -88,6 +91,61 @@ namespace week7
             }
 
             return deathProbability;
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                where x.Gender == person.Gender && x.Age == age
+                select x.P).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                    where x.Age == age
+                    select x.P).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
+
+        private void startSim(int endYear)
+        {
+            for (int year = 2005; year <= endYear; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(year, Population[i]);
+                }
+
+                int nbrOfMales = (from x in Population
+                    where x.Gender == Gender.Male && x.IsAlive
+                    select x).Count();
+                int nbrOfFemales = (from x in Population
+                    where x.Gender == Gender.Female && x.IsAlive
+                    select x).Count();
+
+                popData.Add(new PopulationData()
+                {
+                    femalePop = nbrOfFemales,
+                    malePop = nbrOfMales,
+                    year = year
+                });
+            }
         }
     }
 }
