ac72cd96eddf2e952c1cd7e890bfb6c2dadde10b
oszlanyikornel <kornel.oszlanyi@stud.uni-corvinus.hu>
2020-11-15 23:17:14 +0100

    Added timers

--- week08/Form1.Designer.cs
+++ week08/Form1.Designer.cs
@@ -29,12 +29,47 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.createTimer = new System.Windows.Forms.Timer(this.components);
+            this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // createTimer
+            // 
+            this.createTimer.Enabled = true;
+            this.createTimer.Interval = 300;
+            this.createTimer.Tick += new System.EventHandler(this.createTimer_Tick_1);
+            // 
+            // conveyorTimer
+            // 
+            this.conveyorTimer.Enabled = true;
+            this.conveyorTimer.Interval = 1000;
+            this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick_1);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.Location = new System.Drawing.Point(2, 113);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(1510, 557);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1519, 868);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer createTimer;
+        private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Panel mainPanel;
     }
 }
 
