8ba97e21499b87291d99bf39fba151a48aab0226
oszlanyikornel <kornel.oszlanyi@stud.uni-corvinus.hu>
2020-11-22 19:30:48 +0100

    Added password validation test

--- week09/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ week09/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -28,5 +28,25 @@ namespace UnitTestExample.Test
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+            Test,
+            TestCase("Jelszo", false),
+            TestCase("JELSZO123", false),
+            TestCase("jelszo123", false),
+            TestCase("jel", false),
+            TestCase("Jelszo123", false)
+        ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
